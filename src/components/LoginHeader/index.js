62c115479e373f6659e650a7806efe6ca3d9ca37
ThinhNgo767 <thinhngo767@gmail.com>
2023-10-06 22:05:04 +0700

    fix header

---
+++ src/components/LoginHeader/index.js
@@ -0,0 +1,65 @@
+
+import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const LoginHeader = () => {
+  const [openLogin, setOpenLogin] = useState(false);
+  const loginRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (loginRef.current && !loginRef.current.contains(e.target)) {
+        setOpenLogin(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside, true);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  const openFromLogin = () => {
+    setOpenLogin(!openLogin);
+  };
+
+  return (
+    <div className="login_header" ref={loginRef}>
+      {openLogin ? (
+        <>
+          <button
+            type="button"
+            className="header_button_login header_button--style"
+            onClick={openFromLogin}
+          >
+            ĐĂNG NHẬP
+          </button>
+          
+            <form className="from-login">
+              <input type="text" placeholder="Email"/>
+              <input type="text" placeholder="Password"/>
+              <div className="form-login-item">
+                <button type="button" className="form-button-login header_button--style">ĐĂNG NHẬP</button>
+                <Link to="/reset-password">Quên mật khẩu</Link>
+              </div>
+              <button type="button" className="form-button-sigup header_button--style">ĐĂNG KÝ THÀNH VIÊN</button>
+            </form>
+         
+        </>
+      ) : (
+        <>
+          <button
+            type="button"
+            className="header_button_login header_button--style"
+            onClick={openFromLogin}
+          >
+            ĐĂNG NHẬP
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default LoginHeader;
--- src/pages/Home/component/Banner/settingSideBanner.js
+++ src/pages/Home/component/Banner/settingSideBanner.js
@@ -1,11 +1,10 @@
 const settingsBanner = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: false,
-    
-  };
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+};
 
-  export default settingsBanner
+export default settingsBanner;
